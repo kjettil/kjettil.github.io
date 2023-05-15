ae94a6bdb0c8cc5ff131190389e7febfac0ace7a
kjettil <Kjettil1@gmail.com>
2023-05-15 14:14:58 +0200

    toevoegen cv

---
+++ data/opdracht3/Data.R
@@ -0,0 +1,22 @@
+work <- tribble(
+  ~title, ~unit, ~startMonth, ~startYear, ~endMonth, ~endYear, ~where, ~detail,
+  "Laboratorium analist", " UMC Utrecht", "Juni", 2020, "Heden", NA, "Nederland - Utrecht", "Virologie en covid diagnostiek",
+  "Laboratorium analist", " UMC Utrecht", "Juni", 2020, "Heden", NA, "Nederland - Utrecht", "Gebruikte technieken --- qPCR")
+
+education <- tribble(
+  ~title, ~unit, ~startMonth, ~startYear, ~endMonth, ~endYear, ~where, ~detail,
+  "Mavo", "Farel College", NA, 2012, NA, 2016, "Nederland - Amersfoort", NA,
+  "Biomedisch analist (MBO)", "ROC Midden Nederland", NA, 2016, NA, 2020, "Nederland - Utrecht", "specialisatie vakken --- Klinische Chemie, Moleculaire biologie, microbiologie",
+  "Biomedisch analist (MBO)", "ROC Midden Nederland", NA, 2016, NA, 2020, "Nederland - Utrecht", "Ook ervaring met --- Patholgie, Histologie",
+  "Life science (HBO)", "Hogeschool Utrecht", NA, 2020, "Heden", NA, "Nederland - Utrecht", "specialisatie --- microbiologie",
+  "Life science (HBO)", "Hogeschool Utrecht", NA, 2020, "Heden", NA, "Nederland - Utrecht", "Gebruikte technieken specialisatie --- Maldi-TOF, VITEK2, qPCR",
+  "Life science (HBO)", "Hogeschool Utrecht", NA, 2020, "Heden", NA, "Nederland - Utrecht", "Vakken specialisatie --- Medische microbiologie, Moleculaire biologie, Moleculaire epidemiologie, Virologie, Parasitologie, Research stratagies in microbiology",
+  "Life science (HBO)", "Hogeschool Utrecht", NA, 2020, "Heden", NA, "Nederland - Utrecht", "Minor --- Data Sciences for Biology",
+  "Life science (HBO)", "Hogeschool Utrecht", NA, 2020, "Heden", NA, "Nederland - Utrecht", "Gebruikte talen --- Bash, R")
+
+stage <- tribble(
+  ~title, ~unit, ~startMonth, ~startYear, ~endMonth, ~endYear, ~where, ~detail,
+  "Parasitologie", "GD Diergezondheid/Royal GD", "September", 2018, "Februari", 2019, "Nederland - Deventer", "Half jaar stafe gelopen op de afdeling Parasitologie. Hierbij vooral differentiatie van verschillende parasitaire eieren onder de microscoop uitgevoerd." ,
+  "Parasitologie", "GD Diergezondheid/Royal GD", "September", 2018, "Februari", 2019, "Nederland - Deventer", "Gebruikte technieken --- McMaster, Microscopie " ,
+  "Afstudeerstage MBO", "UMC Utrecht", "September", 2020, "Mei", 2020, "Nederland - Utrecht", "Afdeling virologie",
+  "Afstudeerstage MBO", "UMC Utrecht", "September", 2020, "Mei", 2020, "Nederland - Utrecht", "Gebruikte technieken --- qPCR")
